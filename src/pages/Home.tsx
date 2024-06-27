92c54a1385a8f6976772761c502a5313b84b4497
Aditya <adityakumarji74@gmail.com>
2024-06-28 00:54:25 +0530

    task done

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import Section1 from "../components/Section1";
 import Section2 from "../components/Section2";
-import Footer from "../components/Footer";
 
 const Home: React.FC = () => {
   return (
     <>
       <Section1 />
       <Section2 />
-      <Footer />
     </>
   );
 };
